3cbd68d02e15cc16cacc691ac741c19b7bde183b
weldeon <50535591+weldeondotwav@users.noreply.github.com>
2023-01-22 18:59:25 -0600

    some comments

--- app.go
+++ app.go
@@ -21,15 +21,18 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
+// enum for the card suits
 type CardSuit int
 
 const (
-	Hearts CardSuit = 0
+	Hearts   CardSuit = 0
 	Diamonds CardSuit = 1
-	Clubs CardSuit = 2 
-	Spades CardSuit = 3
+	Clubs    CardSuit = 2
+	Spades   CardSuit = 3
 )
 
+// TODO naming on these is bad
+// Can be indexed by card value to get a string representation
 var cardValueWords = [...]string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 var cardValueDisplayStrings = [...]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
@@ -49,6 +52,7 @@ func (a *App) CardValueToWord(cv int) string {
 	return cardValueWords[cv-1]
 }
 
+// Struct that represents a single playing card
 type Card struct {
 	Value int
 	Suit  CardSuit
@@ -70,6 +74,7 @@ func (a *App) MakeRandomCard() Card {
 
 }
 
+// Struct that holds a collection of cards
 type Deck struct {
 	Cards []Card
 }
@@ -83,16 +88,17 @@ func (a *App) NewStandardDeck() Deck {
 	outDeckContainer := []Card{}
 
 	// each value
+	// Values 11, 12, 13 are Jack, Queen, King, respectively.
 	for val := 1; val < 14; val++ {
 		// each suit
 		for suit := 0; suit < 4; suit++ {
 			outDeckContainer = append(outDeckContainer, Card{
-					Value: val,
-					Suit: CardSuit(suit),
-					IsRed: suit < 2,
-				})
-			}
-		}	
+				Value: val,
+				Suit:  CardSuit(suit),
+				IsRed: suit < 2,
+			})
+		}
+	}
 
 	outDeck.Cards = outDeckContainer
 
